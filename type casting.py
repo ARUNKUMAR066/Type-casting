c2449b3f6e611a49c2cf9ad8fe4b85a33222c27c
ARUNKUMAR066 <118959692+ARUNKUMAR066@users.noreply.github.com>
2022-11-28 00:27:04 +0530

    Add files via upload

---
+++ type casting.py
@@ -0,0 +1,12 @@
+pi=3.14
+print(type(pi))
+num=int(pi)
+print('integer number: ',num)
+num1=20
+print(type(num1))
+num2=float(num1)
+print('float number:',num2)
+num3=(4!=3)
+print(type(num3))
+num4=complex(num3)
+print('complex number:',num4)
